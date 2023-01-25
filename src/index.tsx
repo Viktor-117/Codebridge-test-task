9b115cf70c13c8a000144e37edb0ab733ea58665
Viktor-117 <viktor.hushchin@gmail.com>
2023-01-25 13:33:59 +0200

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -13,7 +13,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename="/Codebridge_test_task/">
+      <BrowserRouter basename="Codebridge_test_task">
         <App />
       </BrowserRouter>
     </Provider>
