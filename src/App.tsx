de2e26d53404252fd0af74bbd53c20f9efdb72f5
Viktor-117 <viktor.hushchin@gmail.com>
2023-01-25 10:09:09 +0200

    add NotFound

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-// import logo from "./logo.svg";
-// import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import { lazy } from "react";
 
 const HomeView = lazy(() => import("./pages/HomeView"));
 const ArticlesList = lazy(() => import("components/ArticlesList"));
 const ArticleView = lazy(() => import("./pages/ArticleView"));
+const NotFound = lazy(() => import("components/NotFound"));
 
 function App() {
   return (
@@ -13,24 +12,9 @@ function App() {
       <Route path="/" element={<HomeView />}>
         <Route index element={<ArticlesList />} />
         <Route path="/articles/:articleId" element={<ArticleView />} />
+        <Route path="*" element={<NotFound />} />
       </Route>
     </Routes>
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.tsx</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
   );
 }
 
